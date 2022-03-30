bb95a29e397720604a6672591d56f42aa32a5723
* <luizfelip079@gmail.com>
2022-03-30 13:47:18 -0300

    -

--- config/enviroment.rb
+++ config/enviroment.rb
@@ -1,17 +1,15 @@
 require 'bundler/setup'
 Bundler.require
-configure :development do
- ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler/setup'
  Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.db"
+  :database => "db/db.db"
  )
-end
 
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+ db = URI.parse(ENV['HEROKU_POSTGRESQL_CYAN_URL'] || 'postgres:///localhost/mydb')
 
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
