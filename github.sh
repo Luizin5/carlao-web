b86cec076f55d57701ddd89edaae29eeb0041795
Luizin5 <83879368+Luizin5@users.noreply.github.com>
2022-06-24 10:38:25 -0300

    Delete github.sh

--- github.sh
+++
@@ -1,7 +0,0 @@
-echo "# carlao-web" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Luizin5/carlao-web.git
-git push -u origin main
